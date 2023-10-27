51d6eb8b66346145902aa11951657ce5210d8184
Mauro Curcuma <108034307+Mauro-Carcamo@users.noreply.github.com>
2023-10-27 04:18:39 -0400

    prueb9

--- config/settings.py
+++ config/settings.py
@@ -118,10 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '../nadar/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS =[os.path.join(BASE_DIR,"nadar/static")]
+STATICFILES_DIRS =[os.path.join(BASE_DIR,"../nadar/static")]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles","static")
